67a5d641d0fb09a590a6b732c936377868a6a89b
crydsch <crydsch@lph.zone>
2023-09-11 23:22:30 +0200

    Set debug logging level to trace

--- src/main.cpp
+++ src/main.cpp
@@ -52,7 +52,7 @@ int main(int argc, char** argv) {
     utils::RNG::init();
 
 #if DEBUG
-    logger::setup_logger(spdlog::level::debug);
+    logger::setup_logger(spdlog::level::trace);
 #else
     logger::setup_logger(spdlog::level::info);
 #endif
